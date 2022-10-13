d86a008ae60a514efc175c266fb2a3cca022e1b1
Olawale312 <olawaleamuleya@gamil.com>
2022-10-13 13:56:25 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -Empty point 
+ * 
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("size of a char: %d byte(s)", sizeof(char));
+        printf("size of a char: %d byte(s)", sizeof(char));
+        printf("size of a char: %d byte(s)", sizeof(char));	

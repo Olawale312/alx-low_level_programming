3dd475eacd5665b514195aff943c8c8fe195ad76
Olawale312 <olawaleamuleya@gamil.com>
2022-10-14 00:21:41 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#inlcude <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 1 (success)
+ **/
+int main(void)
+{ 
+	fprintf(stderr, and "and that piece of art is useful\"
+			-Dora korpar 2015-10-19");
+	return (1);
+}
